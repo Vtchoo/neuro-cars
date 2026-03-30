00d11b9b28fde7de265b6070885606a14b18feaa
Vtchoo <vittor-limma@hotmail.com>
2026-03-30 09:57:50 -0300

    set activation functions in separate file, add signedLog

--- src/utils/activationFunctions.ts
+++ src/utils/activationFunctions.ts
@@ -1,3 +1,29 @@
+export function identity(value: number) {
+    return value;
+}
+
+export function binary(value: number) {
+    return value > 0 ? 1 : 0;
+}
+
+export function relu(value: number) {
+    return value < 0 ? 0 : value;
+}
+
+export function tanh(value: number) {
+    return Math.tanh(value);
+}
+
+export function sigmoid(value: number) {
+    return 1 / (1 + Math.exp(-value));
+}
+
 export function softsign(value: number) {
     return value / (1 + Math.abs(value));
+}
+
+export function signedLog(value: number) {
+    if (value === 0) return 0;
+    const sign = Math.sign(value);
+    return sign * Math.log1p(Math.abs(value));
 }
