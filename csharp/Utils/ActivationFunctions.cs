b8e72c8de847690fb23ff8bf64fc8051abde49e1
Vtchoo <vittor-limma@hotmail.com>
2026-03-31 20:29:54 -0300

    create activation functions file

---
+++ csharp/Utils/ActivationFunctions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace SmartRace.Utils
+{
+    internal static class ActivationFunctions
+    {
+        public static double Identity(double value) => value;
+
+        public static double Binary(double value) => value > 0 ? 1 : 0;
+
+        public static double ReLU(double value) => value < 0 ? 0 : value;
+
+        public static double Tanh(double value) => Math.Tanh(value);
+
+        public static double Sigmoid(double value) => 1 / (1 + Math.Exp(-value));
+
+        public static double Softsign(double value) => value / (1 + Math.Abs(value));
+
+        public static double SignedLog(double value)
+        {
+            if (value == 0) return 0;
+            double sign = Math.Sign(value);
+            return sign * Math.Log(1 + Math.Abs(value));
+        }
+    }
+}
